82d50b025c6e68a48b668ae24c7460ec1c65566d
Sim <simranlitt@gmail.com>
2015-12-06 23:18:39 -0800

    first commit

---
+++ MeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeMe
+//
+//  Created by Parabsimran Litt on 12/6/15.
+//  Copyright © 2015 Parabsimran Litt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
